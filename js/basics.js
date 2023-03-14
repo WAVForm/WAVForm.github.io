5fd5ea686d6c0669f884ef5f7b24b6f0e2937ed2
Yunis Nabiyev <yunisnabiyev2@gmail.com>
2023-03-14 13:08:17 -0700

    Generalized shared CSS elements into main.css, added colors to pallete, fleshed out home page (to be completed)

--- js/basics.js
+++ js/basics.js
@@ -4,11 +4,16 @@ $(document).ready(()=>{
     $('#mobile-open').css("display", "block");
     $('#mobile-open').css("animation", "nav-menu-opening 1s cubic-bezier(0.5, 0, 0.25, 1) 1");
     $('#mobile-open').css("width", "var(--mobile-menu-size)");
+    setTimeout(()=>{
+      $('#mobile-open').css("min-width", "125px");
+    }, "1000");
+
   });
 
   $('#mobile-exit').click( ()=>{
     $('#mobile-open').css("display", "block");
     $('#mobile-open').css("animation", "");
+    $('#mobile-open').css("min-width", "");
     setTimeout(()=>{
     $('#mobile-open').delay(1).css("animation", "nav-menu-opening 1s cubic-bezier(0.5, 0, 0.25, 1) 1 reverse");
     setTimeout(() => {
