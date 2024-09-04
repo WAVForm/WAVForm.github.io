e42a1d8e0fddb77044ca70f88e599c78d77e7813
Yunis Nabiyev <35284938+WAVForm@users.noreply.github.com>
2024-09-03 19:19:33 -0700

    removed erroneous ids, open menu, age calc, started skills table

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+var today = new Date();
+var bday = new Date(2002,1,15);
+var millisecsInYear = 365*24*60*60*1000;
+var age = parseInt((today - bday)/millisecsInYear);
+document.getElementById("about-me-age").innerHTML = age;
+
 document.getElementById("menu-open").addEventListener("click", (event)=>{
-    console.log("Open");
+    document.getElementById("menu-dropdown").style.display = "block";
 });
 document.getElementById("menu-close").addEventListener("click", (event)=>{
-    console.log("Close");
+    document.getElementById("menu-dropdown").style.display = "none";
 });
 for(var elem of document.getElementsByClassName("sub-section-container")){
     let tempElem = elem;
