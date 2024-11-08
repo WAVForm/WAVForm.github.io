6bb2a34834615753b5643a69622670da27806cf0
Yunis Nabiyev <35284938+WAVForm@users.noreply.github.com>
2024-11-08 00:32:41 -0800

    designed

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 /*shared*/
 var theme_toggle = document.getElementById("menu_settings_theme_toggle");
+var menu = document.getElementById("menu");
+var menu_button = document.getElementById("menu_button");
 toggle_theme();
 
 theme_toggle.addEventListener("change", toggle_theme);
+menu_button.addEventListener("click", toggle_menu);
+menu_button.addEventListener("touchstart", (e) => { e.preventDefault(); toggle_menu(); });
+for (let button of document.getElementsByClassName("menu_button")) {
+    button.addEventListener("click", toggle_menu);
+}
+for (let drop of document.getElementsByClassName("section_dropdown_title")) {
+    drop.addEventListener("click", () => {
+        let sibling = drop.nextElementSibling;
+        if (!sibling.checkVisibility({ checkVisibilityCSS: true })) {
+            sibling.style.height = "auto";
+            sibling.style.visibility = "visible";
+            let coords = sibling.getBoundingClientRect();
+            window.scrollTo(0, coords.y + window.scrollY - coords.height);
+        }
+        else {
+            sibling.style.height = "0px";
+            sibling.style.visibility = "hidden";
+            let coords = drop.getBoundingClientRect();
+            window.scrollTo(0, window.scrollY - coords.height);
+        }
+    });
+}
+for (let drop of document.getElementsByClassName("sub_section_dropdown_title")) {
+    drop.addEventListener("click", () => {
+        let sibling = drop.nextElementSibling;
+        if (!sibling.checkVisibility({ checkVisibilityCSS: true })) {
+            sibling.style.height = "250px";
+            sibling.style.visibility = "visible";
+            console.log(sibling.getBoundingClientRect() + window.scrollY);
+        }
+        else {
+            sibling.style.height = "0px";
+            sibling.style.visibility = "hidden";
+            console.log(drop.getBoundingClientRect() + window.scrollY);
+        }
+    });
+}
 
-function toggle_theme(){
+function toggle_theme() {
     var currentTheme = document.documentElement.getAttribute("display_mode");
     var targetTheme = "dark";
 
@@ -14,10 +53,16 @@ function toggle_theme(){
 
     document.documentElement.setAttribute('display_mode', targetTheme)
 }
-/*shared*/
 
-let today = new Date();
-let bday = new Date(2002,2,15);
-let millisecsInYear = 365*24*60*60*1000;
-let age = parseInt((today - bday)/millisecsInYear);
-//document.getElementById("about-me-age").innerHTML = age;
+let menu_open = false;
+function toggle_menu() {
+    if (menu_open) {
+        menu.style.visibility = "hidden";
+        menu_open = false;
+    }
+    else {
+        menu.style.visibility = "visible";
+        menu_open = true;
+    }
+}
+/*shared*/
