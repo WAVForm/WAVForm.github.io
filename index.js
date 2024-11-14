e89aa9db556b899274097d4de0b2411d0873b4f8
Yunis Nabiyev <35284938+WAVForm@users.noreply.github.com>
2024-11-14 15:39:32 -0800

    added icons, removed old ones
    fixed theme toggle
    
    Added icons, optimized with https://yqnn.github.io/svg-path-editor/

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 var theme_toggle = document.getElementById("menu_settings_theme_toggle");
 var menu = document.getElementById("menu");
 var menu_button = document.getElementById("menu_button");
-document.documentElement.setAttribute("display_mode", localStorage.getItem('themePreference'));
-toggle_theme(true);
+document.documentElement.setAttribute("display_mode", localStorage.getItem('themePreference') === "dark" ? "dark" : "light");
 
 theme_toggle.addEventListener("change", toggle_theme);
 menu_button.addEventListener("click", toggle_menu);
@@ -52,19 +51,9 @@ for (let drop of document.getElementsByClassName("sub_section_dropdown_title")) 
 
 function toggle_theme(f) {
     var currentTheme = document.documentElement.getAttribute("display_mode");
-    var targetTheme = "dark";
+    var targetTheme = currentTheme === "dark" ? "light" : "dark";
     var ball = document.getElementById("toggle_ball");
-    ball.style.left = "0px";
-
-    if (currentTheme === "dark") {
-        targetTheme = "light";
-        ball.style.left = "50px";
-    }
-
-    if (f == true) {
-        ball.style.left = ball.style.left == "50px" ? "0px" : "50px";
-        return;
-    }
+    ball.style.left = ball.style.left === "50px" ? "0px" : "50px";
     document.documentElement.setAttribute('display_mode', targetTheme)
     localStorage.setItem('themePreference', targetTheme);
 }
